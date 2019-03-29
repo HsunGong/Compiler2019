82ddc19e2923057908c461806d64666c34d4d27e
xun <gongxuncd@gmail.com>
2019-03-29 21:47:56 +0800

    test MNull

--- src/main/java/mxcompiler/type/MNullType.java
+++ src/main/java/mxcompiler/type/MNullType.java
@@ -1,10 +1,11 @@
 package mxcompiler.type;
 
-/** UGLY: MNullType to specify from NullType(which may be returnType)
- * MNull means "null", but Null means ""
+/**
+ * UGLY: MNullType to specify from NullType(which may be returnType) MNull means
+ * "null", but Null means ""
  */
 public class MNullType extends Type {
-    public MNullType() {
-		super(Type.InnerType.MNULL);
-    }
+  public MNullType() {
+    super(Type.InnerType.NULL);
+  }
 }
